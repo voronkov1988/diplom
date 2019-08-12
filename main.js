8a2b551c0b8e84ab2dd05098f1ac0f7ee3a1fbcd
voronkov88 <rybsoveti.ru@yandex.ru>
2019-08-12 20:43:40 +0300

    доработки модалок

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
 'use strict';
 
 //перезвони мне-------------------------------------------------------------------
-const tooglePopup = () => {
-    const tellMe = document.querySelectorAll('.call-btn'),
-        popupCall = document.querySelector('.popup-call'),
+const tooglePopup = (modal, button) => {
+    const popupCall = document.querySelector(modal),
+        body = document.querySelector('body'),
         popup = document.querySelector('.popup'),
-        popupContent = document.querySelector('.popup-content'),
-        mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        popupContent = document.querySelector('.popup-content');
 
-    tellMe.forEach((elem) => {
-        elem.addEventListener('click', () => {
-            if (mobile) {
-                popupCall.style.display = 'block';
-            }else{
-                popupCall.style.display = 'block';
-                let topPosition = 110 - popupCall.clientWidth;
-                let animation = () => {
-                    popupContent.style.top = `${topPosition}px`;
-                    if (popupContent.getBoundingClientRect().top < 100) {
-                        topPosition += 10;
-                    }else{
-                        clearInterval(pix);
-                    }
-                };
-                let pix = setInterval(animation, 1);
-            }
-        });
+
+    body.addEventListener('click', (event) => {
+        let target = event.target;
+        let Elem = document.querySelector('body');
+
+        if (target.closest(button)){
+            popupCall.style.display = 'block';
+        }
+        if (target.matches('.popup-close')) {
+            popupCall.style.display = 'none';
+        }
+        if (target.matches('.popup')) {
+            popupCall.style.display = 'none'
+        }
+
+        if (target.closest('body')) {
+            Elem.style.display = 'block';
+        } else {
+            Elem.style.display = 'none';
+        }
     });
     popup.addEventListener('click', (event) => {
+        event.preventDefault();
         let target= event.target;
-        if (target.classList.contains('popup-close')){
+        if (target.classList.contains('.popup-close')){
             popupCall.style.display = 'none';
         }else{
             target = target.closest('.popup-content');
@@ -39,12 +41,16 @@ const tooglePopup = () => {
         }
     });
 };
-tooglePopup();
+tooglePopup('.popup-call', '.construct-btn .call-btn');
+tooglePopup('.popup-discount', '.call-btn');
+tooglePopup('.popup-check', '.discount-btn');
+tooglePopup('.popup-consultation', '.check-btn');
+tooglePopup('.popup-consultation', '.director-btn');
 
 
 //отправка формы------------------------------------------------------
 
-const sendMainForm = (forms) => {
+function sendMainForm (forms) {
     const errorMessage = 'Чтото пошло не так',
         loadMessage = 'загрузка',
         succesMessage = 'Спасибо! мы скоро с вами свяжемся!';
@@ -96,11 +102,16 @@ const sendMainForm = (forms) => {
             body: JSON.stringify(body),
         });
     };
-};
+}
 sendMainForm('.main-form');
 sendMainForm('.director-form');
 sendMainForm('.capture-form');
-sendMainForm('.popup-call .capture-form');
+sendMainForm('.popup-call');
+sendMainForm('.popup-consultation .capture-form');
+sendMainForm('.popup-check .capture-form');
+sendMainForm('.popup-discount .capture-form');
+
+
 
 
 
@@ -236,6 +247,7 @@ const calc = () => {
         }
     });
     getResult.addEventListener('click', (event)=>{
+        event.preventDefault();
         if (lastInput){
             let metr = lastInput.value;
             lastInput.setAttribute('name_allMetres', metr);
@@ -245,7 +257,6 @@ const calc = () => {
             total = total * col1 * col2 * diametr1 * diametr2 + dno;
             calcResult.placeholder = total;
             calcResult.setAttribute('name_calcCost', total);
-            console.log(calcResult);
         }
         const formData = new FormData(calcResult, lastInput);
         let body = {};
@@ -266,4 +277,20 @@ const calc = () => {
     };
 calc();
 
+//Дополнительные блоки
 
+const  addBlock = () => {
+    const addSentenceBtn = document.querySelector('.add-sentence-btn'),
+        shadowBlock = document.querySelectorAll('.sentence .shadow-block'),
+        sentence = document.querySelector('.sentence');
+
+    addSentenceBtn.addEventListener('click', ()=>{
+        if (addSentenceBtn){
+            for (let i = 0; i < shadowBlock.length; i++){
+                shadowBlock[i].classList.remove('hidden');
+                console.log('kick');
+            }
+        }
+    });
+};
+addBlock();
